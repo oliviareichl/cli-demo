2d0279d9b6d36cc46f5ada461f49584fbd2f948e
oliviareichl <Olivia.Reichl@oeaw.ac.at>
2023-01-12 14:59:04 +0100

    Event created

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,71 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="event in events" :key="event.id">
+  <Event :event="event" :date="daysLeft(event)">
+  </Event>
+    </li>
+  </ul>
 </template>
+
+<script>
+import Event from './components/Event.vue'
+
+const eventData = [
+  {
+    id: 1,
+    name: 'Graduation',
+    date: '2016-06-30',
+    details: 'wow',
+    background: '#c9ffaa'
+  },
+  {
+    id: 2,
+    name: 'Birthday',
+    date: '2023-10-07',
+    details: 'party',
+    background: '#bf5b63'
+  },
+  {
+    id: 3,
+    name: 'Christmas',
+    date: '2023-12-24',
+    details: 'hoho',
+    background: '#fff8aa'
+  },
+  {
+    id: 4,
+    name: 'Weekend!',
+    date: '2023-01-13',
+    details: 'juhu!',
+    background: '#e3aaff'
+  },
+  {
+    id: 5,
+    name: 'Musical',
+    date: '2023-01-12',
+    details: 'drinking!',
+    background: '#aac8ff'
+  },
+]
+export default {
+  components: {
+    Event,
+  },
+  data() {
+    return {
+      events: eventData,
+    }
+  },
+  methods: {
+    daysLeft(event) {
+      const Time = Date.parse(event.date) - Date.now();
+      const Days = Math.round(Time / (1000 * 60 * 60 * 24));
+      return Days;
+
+    }
+  },
+}
+</script>
 
 <style scoped>
 header {
@@ -23,6 +75,10 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+li {
+  list-style: none;
 }
 
 @media (min-width: 1024px) {
---
+++ src/components/Event.vue
@@ -0,0 +1,41 @@
+<template>
+  <article :style="{background: event.background}">
+    <div class="name">
+      <h3> {{ event.name }} </h3>
+      <br />
+      <p> {{ event.details }} </p>
+    </div>
+    <div class="date">
+    <p v-if="date > 0"> {{ date }} days left </p>
+      <p v-if="date === 0">Today!</p>
+      <p v-if="date < 0"> {{ Math.abs(date) }} days ago </p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['event', 'date'],
+}
+</script>
+
+<style scoped>
+
+article {
+  background: #2c3e50;
+  display: flex;
+  margin: 2rem;
+  border-radius: 0.3rem;
+}
+
+.name {
+  color: whitesmoke;
+
+}
+
+.date {
+  color:whitesmoke;
+  margin-left: 2rem;
+
+}
+</style>
